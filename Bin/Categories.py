017940012cf401d4a401bfa20dff54aaf009958c
dragonpaulz <paul.geesaman@gmail.com>
2015-12-09 21:15:53 -0500

    Added logging to file
    Categories and sentences now open files to write the messages.

--- Bin/Categories.py
+++ Bin/Categories.py
@@ -5,5 +5,7 @@ LOGNAME="categories.log"
 class Categories:
     mapWordsToCategories = []
     def __init__(self, categoriesFilename):
+        categoriesLog = open(LOGNAME, 'w')
         categoriesFile = xlrd.open_workbook(filename=categoriesFilename, \
-            verbosity=10)
+            verbosity=10, logfile = categoriesLog)
+        categoriesLog.close()
--- Bin/Sentences.py
+++ Bin/Sentences.py
@@ -5,5 +5,7 @@ LOGNAME="sentences.log"
 class Sentences:
     sentenceToParse = []
     def __init__(self, sentencesFilename):
+        sentencesLog = open(LOGNAME, 'w')
         sentencesFile = xlrd.open_workbook(filename=sentencesFilename, \
-            verbosity=10)
+            verbosity=10, logfile = sentencesLog)
+        sentencesLog.close()
