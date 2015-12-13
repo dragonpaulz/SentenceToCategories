44446fd1a389b22fbe4daaf0eb47bea115c48045
dragonpaulz <paul.geesaman@gmail.com>
2015-12-13 16:34:07 -0500

    Setting up output file

--- Bin/main.py
+++ Bin/main.py
@@ -1,6 +1,7 @@
 import Categories
 import Sentences
 import logging
+import csv
 
 #configuration:
 CATEGORIESFILE="..\Data\Categories.csv"
@@ -8,13 +9,21 @@ SENTENCESFILE="..\Data\Sentences.csv"
 LOGGERFILE="ParserLog.log"
 LOGGERDETAIL=logging.INFO
 FORMAT="Level:%(levelno)s\tModule:%(module)s\tLine:%(lineno)d\t%(message)s"
+OUTPUTFILE="..\OutputFile.csv"
 
 def main():
     logging.basicConfig(filename=LOGGERFILE, level=LOGGERDETAIL, format=FORMAT)
     logger = logging.getLogger(LOGGERFILE)
     logging.info("Process begins")
     categories = Categories.Categories(CATEGORIESFILE)
+    logging.info("Completed creating categories object")
     sentences = Sentences.Sentences(SENTENCESFILE)
+    logging.info("Completed extracting sentences")
+    createOutputFile()
+    
+def createOutputFile():
+    with open(OUTPUTFILE, 'w') as csvOut:
+        output = csvOut.write(csvOut, delimeter = ',')
 
 if(__name__=='__main__'):
     main()
